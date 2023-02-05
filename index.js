f4912dedb18e290820bdf4d3dc2f7b90414b58b9
Caroline O <me@carolineoliver.net>
2023-02-05 13:31:40 -0600

    Added MySQL testing.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "replace-manually"
+});
 
 app.get('/', (req, res) => {
     res.end("Hello world!");
@@ -8,4 +15,9 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('WBTT server listening at 3.141.202.74:3000')
+});
+
+con.connect( (err) => {
+    if (err) throw err;
+    console.log("Connected to MySQL Server.");
 });
