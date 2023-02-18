3a0fedcd87b33b11f1cbd0bf3eec2af228893ab6
Caroline O <me@carolineoliver.net>
2023-02-17 18:51:48 -0600

    Committed changes made on-server from 2 last weeks

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-const con = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 10,
     host: "localhost",
     user: "root",
-    password: "replace-manually",
-    database: "mysql_test"
+    password: "UTSACSgroup7",
+    database: "mysql_test",
 });
 
 app.get('/', (req, res) => {
     res.end("Hello world!");
 });
 
+app.use('/static', express.static('html'));
+
 app.get('/getall', (req, res) => {
-    con.query("SELECT * FROM test_table1", function (err, result, fields) {
+    pool.query("SELECT * FROM test_table1", function (err, result, fields) {
         if (err) throw err;
         res.end(JSON.stringify(result));
   });
@@ -25,7 +28,3 @@ app.listen(port, () => {
     console.log('WBTT server listening at 3.141.202.74:3000')
 });
 
-con.connect( (err) => {
-    if (err) throw err;
-    console.log("Connected to MySQL Server.");
-});
