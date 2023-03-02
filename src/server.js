c06342ec0ad6f91565f376bc09b886d2ce9ec738
Caroline O <me@carolineoliver.net>
2023-03-02 14:56:05 -0600

    fixed login token retrival

--- src/server.js
+++ src/server.js
@@ -1,22 +1,13 @@
 // #region authenticate middleware
-
-/*
-{
-	token: "tokenjadkfljds"
-}
-*/
-
-//   pool.query("SELECT * FROM test_table1", function (err, result, fields) {
-//         if (err) throw err;
-//         res.end(JSON.stringify(result));
-//   });
-
 function authenticate(req, res, next) {
 	if (req.body.token == null) {
 		res.status(400).send("Token not present");
     }
     else {
-        pool.query("SELECT * FROM user WHERE user_id = " + req.body.token + ";", function(err, result, fields) {
+        // parameterized MySQL requests are immune to SQL injection
+        var sql = "SELECT * FROM user WHERE user_id = ?;"
+        sqlParams = [req.body.token];
+        pool.query(sql, sqlParams, function(err, result) {
             if (err) throw err;
             else if (result.length == 0) {
                 res.status(404).send("Token not valid");
@@ -93,12 +84,6 @@ app.get('/', (req, res) => {
 // #endregion
 
 // #region user account api
-app.get('/my/auth', authenticate, (req, res) => {
-    res.status(200).send("hello world");
-});
-
-const users = [{user: "admin", password: "password123"}];
-
 app.post('/my/create', (req, res) => {
     // make sure request contains all elements of a user account
     if (req.body.username == null || req.body.password == null
@@ -107,17 +92,44 @@ app.post('/my/create', (req, res) => {
         res.status(403).send("Missing body parts");
         return;
     }
-    
     // make sure user account doesn't already exist
-    if ( (users.some( ({user}) => user === req.body.username ) ) ) {
-        res.status(403).send("Username already exists");
-        return;
-    }
-
-    users.push({user: req.body.username, password: req.body.password});
+    var sql = "SELECT * FROM user WHERE user_name = ?;"
+    sqlParams = [req.body.username];
+    pool.query(sql, sqlParams, function(err_01, result_01) {
+        if (err_01) throw err_01;
+        else if (result_01.length != 0) {
+            res.status(400).send("Username already in user");
+        }
+        else {
+            var max_id = null;
+            var sql = "SELECT MAX(user_id) AS max_id FROM user";
+            sqlParams = [];
+            pool.query(sql, sqlParams, function(err_0, result_0) {
+                if (err_0) throw err;
+        
+                max_id = result_0[0].max_id + 1;
+        
+                // inserts new user into user table
+                sql = "INSERT INTO user (user_id, user_name, first_name, last_name, email, type) values (?, ?, ?, ?, ?, 1)";
+                sqlParams = [max_id, req.body.username, req.body.first_name, req.body.last_name, req.body.email];
+                pool.query(sql, sqlParams, function(err_1, result_1) {
+                    if (err_1) throw err_1;
+        
+                    sql = "INSERT INTO password (password_id, user_name, password) values (?, ?, ?)";
+                    sqlParams = [max_id, req.body.username, req.body.password];
+                    pool.query(sql, sqlParams, function(err_2, result_2) {
+                        if (err_2) throw err_2;
+        
+                        res.status(200).send("Account successfully created!");
+                    });
+                });
+            });
+        }
+    });
+    
 });
 
-app.get('/user/login', (req, res) => {
+app.get('/my/login', (req, res) => {
     // make sure request contains all elements of a user account
     if (req.body.username == null || req.body.password == null) {
         res.status(403).send("Missing body parts");
@@ -125,33 +137,42 @@ app.get('/user/login', (req, res) => {
     }
 
     // checks to see if username/password pair exist
-    if (!users.some( ({user,password}) => user === req.body.username && password === req.body.password)) {
-        res.status(403).send("Username/password pair does not exist: " + req.body.username + ", " + req.body.password);
-        return;
-    }
+    var sql = "SELECT * FROM password WHERE user_name = ? AND password = ?"
+    var sqlParams = [req.body.username, req.body.password];
+    var sqlResult = null;
+    pool.query(sql, sqlParams, function(err_1, result_1) {
+        if (err_1) {
+            throw err_1;
+        }
+        else if (result_1.length == 0) {
+            res.status(400).send("Invalid username/password combination.");
+        }
+        else {
+            var sql = "SELECT * FROM user WHERE user_name = ?"
+            var sqlParams = [req.body.username];
+            pool.query(sql, sqlParams, function(err_2, result_2) {
+                if (err_2) throw err_2;
+                else if (result_2.length == 0) {
+                    res.status(403).send("DB Error. Please contact an administrator.");
+                }
+                else {
+                    res.status(200).send({token: result_2[0].user_id});
+                }
+            });
+        }
+    });
 
-    // provide access and refresh tokens
-    // TODO: convert to prod code
-    res.status(200).send("Success! Logged in.");
+    
 });
 // #endregion
 
 // #region admin
-app.post('/admin/upload', /*validateToken,*/ (req, res) => {
+app.post('/admin/upload', authenticate, (req, res) => {
     fs.writeFile(`venues/${req.body.name}.html`, req.body.html, (err) => {
         if (err) return console.log(err);
     });
 });
 //#endregion
-
-// #region example query
-// app.get('/getall', (req, res) => {
-//     pool.query("SELECT * FROM test_table1", function (err, result, fields) {
-//         if (err) throw err;
-//         res.end(JSON.stringify(result));
-//   });
-// });
-// #endregion
 
 // #region listen on port
 app.listen(port, () => {
