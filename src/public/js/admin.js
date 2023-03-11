4ffec0b6341d9c91e3e5fba73c5a8af811e0ce7c
Caroline O <me@carolineoliver.net>
2023-03-11 15:10:30 -0600

    re-wrote admin.js

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -1,19 +1,18 @@
 // JavaScript Document
 
 function createUser() {
-	var data = {
-		'\"username\"': document.getElementById('username').value,
-		'\"password\"': document.getElementById('password').value,
-		'\"email\"': document.getElementById('email').value,
-		'\"first_name\"': document.getElementById('first_name').value,
-		'\"last_name\"': document.getElementById('last_name').value
+	headers = {
+		'Content-Type': 'application/json',
 	}
-	fetch("/api/my/create", {
-		method: "POST",
-		header: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
-		body: JSON.stringify(data)
-	})
-		.then(resp => resp.text())
-		.then(data => console.log("Response: " + data))
-	console.log(data);
+
+	json_data = {
+		'username': document.getElementById('username').value,
+		'password': document.getElementById('password').value,
+		'email': document.getElementById('email').value,
+		'first_name': document.getElementById('first_name').value,
+		'last_name': document.getElementById('last_name').value,
+	}
+
+	response = requests.post('http://18.219.2.17:3000/api/my/create', headers = headers, json = json_data)
+	console.log(response)
 }
