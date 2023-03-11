e963caa77055c574e19dd97851c4cd21e05d64b6
Caroline O <me@carolineoliver.net>
2023-03-11 15:02:27 -0600

    changed admin.js

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -2,11 +2,11 @@
 
 function createUser() {
 	var data = {
-		username: document.getElementById('username').value,
-		password: document.getElementById('password').value,
-		email: document.getElementById('email').value,
-		first_name: document.getElementById('first_name').value,
-		last_name: document.getElementById('last_name').value
+		'username': document.getElementById('username').value,
+		'password': document.getElementById('password').value,
+		'email': document.getElementById('email').value,
+		'first_name': document.getElementById('first_name').value,
+		'last_name': document.getElementById('last_name').value
 	}
 	fetch("/api/my/create", {
 		method: "POST",
