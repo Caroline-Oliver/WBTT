0b77df86666eee89c87dc72854705a6bcddd4e47
Caroline O <me@carolineoliver.net>
2023-03-11 15:25:30 -0600

    changed admin.js to dynamic

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -13,24 +13,6 @@ function createUser() {
 	// 	'last_name': document.getElementById('last_name').value,
 	// }
 
-	// response = requests.post('http://18.219.2.17:3000/api/my/create', headers = headers, json = json_data)
-	// console.log(response)
-
-	// const settings = {
-	// 	"async": true,
-	// 	"crossDomain": true,
-	// 	"url": "18.219.2.17:3000/api/my/create",
-	// 	"method": "POST",
-	// 	"headers": {
-	// 	  "Content-Type": "application/json"
-	// 	},
-	// 	"processData": false,
-	// 	"data": `{\n    \"username\" : \"${document.getElementById('username').value}\",\n    \"password\" : \"${document.getElementById('password').value}\",\n    \"email\" : \"${document.getElementById('email').value}\",\n    \"first_name\" : \"${document.getElementById('first_name').value}\",\n    \"last_name\" : \"${document.getElementById('last_name').value}\"\n}\n`
-	//   };
-	  
-	//   $.ajax(settings).done(function (response) {
-	// 	console.log(response);
-	//   });
 	console.log('sending request...');
 
 	const settings = {
@@ -42,7 +24,7 @@ function createUser() {
 		  "Content-Type": "application/json"
 		},
 		"processData": false,
-		"data": "{\n    \"username\" : \"billyBob12333\",\n    \"password\" : \"secretPass123\",\n    \"email\" : \"billybob\",\n    \"first_name\" : \"Billy\",\n    \"last_name\" : \"Bob\"\n}\n"
+		"data": `{\n    \"username\" : \"${document.getElementById('username').value}\",\n    \"password\" : \"${document.getElementById('password').value}\",\n    \"email\" : \"${document.getElementById('email').value}\",\n    \"first_name\" : \"${document.getElementById('first_name').value}\",\n    \"last_name\" : \"${document.getElementById('last_name').value}\"\n}\n`
 	  };
 
 	  console.log(settings);
