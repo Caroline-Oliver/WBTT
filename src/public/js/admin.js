2227a6f978850405ab17e00ed0de951765221041
Caroline O <me@carolineoliver.net>
2023-03-11 15:14:16 -0600

    changed admin.js

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -1,18 +1,34 @@
 // JavaScript Document
 
 function createUser() {
-	headers = {
-		'Content-Type': 'application/json',
-	}
+	// headers = {
+	// 	'Content-Type': 'application/json',
+	// }
 
-	json_data = {
-		'username': document.getElementById('username').value,
-		'password': document.getElementById('password').value,
-		'email': document.getElementById('email').value,
-		'first_name': document.getElementById('first_name').value,
-		'last_name': document.getElementById('last_name').value,
-	}
+	// json_data = {
+	// 	'username': document.getElementById('username').value,
+	// 	'password': document.getElementById('password').value,
+	// 	'email': document.getElementById('email').value,
+	// 	'first_name': document.getElementById('first_name').value,
+	// 	'last_name': document.getElementById('last_name').value,
+	// }
 
-	response = requests.post('http://18.219.2.17:3000/api/my/create', headers = headers, json = json_data)
-	console.log(response)
+	// response = requests.post('http://18.219.2.17:3000/api/my/create', headers = headers, json = json_data)
+	// console.log(response)
+
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://18.219.2.17:3000/api/my/create",
+		"method": "POST",
+		"headers": {
+		  "Content-Type": "application/json"
+		},
+		"processData": false,
+		"data": `{\n    \"username\" : \"${document.getElementById('username').value}\",\n    \"password\" : \"${document.getElementById('password').value}\",\n    \"email\" : \"${document.getElementById('email').value}\",\n    \"first_name\" : \"${document.getElementById('first_name').value}\",\n    \"last_name\" : \"${document.getElementById('last_name').value}\"\n}\n`
+	  };
+	  
+	  $.ajax(settings).done(function (response) {
+		console.log(response);
+	  });
 }
