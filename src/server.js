c3976bae9ee52b6f4dda7ddabd5a65db8633a7ac
Caroline O <me@carolineoliver.net>
2023-03-23 20:52:19 -0500

    maybe fixed checkTimeStamps?

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ function checkTimestamps(search_terms) {
         padL(dt.getSeconds())}`
     
     var sql = "UPDATE ticket SET hold = null, hold_time = null, user_name = null ";
-    sql += "WHERE (" + search_terms + ") AND hold_time > " + datetime + ";";
+    sql += "WHERE (" + search_terms + ") AND hold_time > \'" + datetime + "\';";
 
     return new Promise((resolve, reject) => {
         pool.query(sql, function (err, result) {
