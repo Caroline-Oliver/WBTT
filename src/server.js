fcafdb5187f06d3f6c837edc750c4c86d0c64c9f
Caroline-Oliver <me@carolineoliver.net>
2023-03-28 22:40:07 -0500

    fixed mistake with copy paste

--- src/server.js
+++ src/server.js
@@ -197,6 +197,7 @@ var current_date = ""
 var current_events = []
 function getCurrentEvents() {
     return new Promise((resolve, reject) => {
+        const dt = new Date();
         const padL = (nr, len = 2, chr = `0`) => `${nr}`.padStart(2, chr);
 
         // YYYY-MM-DD format
