0923cfece330e9e0c76c9dbe495a81e5ce00bc2b
Caroline-Oliver <me@carolineoliver.net>
2023-04-07 10:52:06 -0500

    Re-inserted logging function.

--- src/server.js
+++ src/server.js
@@ -210,7 +210,8 @@ function searchEvents(search_terms) {
             } else {
                 sql += 'ORDER BY count_words DESC, date DESC;';
             }
-            
+            console.log(sql);
+
             pool.query(sql, (err, result) => {
                 if (err) {
                     console.log('search function errored\n');
