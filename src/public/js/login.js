f8799e4a07c559fed75247418f9bb37355e0bb5a
Caroline Oliver <me@carolineolive.net>
2023-04-07 18:59:06 -0500

    Refactored logout function.

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -18,8 +18,8 @@ function login() {
 }
 
 function logout() {
-	document.cookie = "token=1; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/";
 	location.replace('/');
+	document.cookie = "token=1; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/";
 }
 
 function eventSearch() {
