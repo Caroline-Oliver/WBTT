0cb139a0d00b56708791cb53c8ec4b0b29de0d17
Caroline-Oliver <me@carolineoliver.net>
2023-04-11 14:08:05 -0500

    Fixed typo

--- src/server.js
+++ src/server.js
@@ -1129,7 +1129,7 @@ app.get('/admin/editUser/:user_id', authenticate, (req, res) => {
                 .then((result) => {
                     res.render('pages/edit-user', {
                         status: loggedIn,
-                        user: result[0]
+                        user: result
                     });
                 })
         });
