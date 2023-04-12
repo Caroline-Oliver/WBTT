70cf0c7d59747c00ee4fe827819162f9e8adb63f
Caroline-Oliver <me@carolineoliver.net>
2023-04-12 11:39:57 -0500

    Further implemented register

---
+++ src/public/js/register.js
@@ -0,0 +1,27 @@
+function register() {
+	var sentinel = -1
+	window.localStorage.setItem('minutes', sentinel);
+	window.localStorage.setItem('seconds', sentinel);
+	
+	$.ajax({
+		url: '/api/my/create',
+		dataType: 'json',
+		type: 'post',
+		contentType: 'application/jsonp',
+		data: JSON.stringify({
+			"username": `${document.getElementById('username').value}`,
+			"password": `${document.getElementById('password').value}`,
+			"email": `${document.getElementById('email').value}`,
+			"first_name": `${document.getElementById('first_name').value}`,
+			"last_name": `${document.getElementById('last_name').value}`
+		}),
+		processData: false,
+		success: function (data) {
+			location.reload();
+		},
+        error: function(data) {
+            // consider doing more sophisticated error messages
+            document.getElementById('last_name').innerHTML = 'Failed to create account';
+        }
+	});
+}
