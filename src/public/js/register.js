852474ab3012a924ff1f7768d65baccefc03731c
Caroline-Oliver <me@carolineoliver.net>
2023-04-12 11:49:03 -0500

    Logging

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -4,27 +4,43 @@ function register() {
 	window.localStorage.setItem('minutes', sentinel);
 	window.localStorage.setItem('seconds', sentinel);
 	
-	$.ajax({
-		url: '/api/my/create',
-		dataType: 'json',
-		type: 'post',
-		contentType: 'application/jsonp',
-		data: JSON.stringify({
-			"username": `${document.getElementById('username').value}`,
-			"password": `${document.getElementById('password').value}`,
-			"email": `${document.getElementById('email').value}`,
-			"first_name": `${document.getElementById('first_name').value}`,
-			"last_name": `${document.getElementById('last_name').value}`
-		}),
-		processData: false,
-		success: function (data) {
-			//location.reload();
-            console.log(data);
-            console.log(JSON.stringify(data));
-		},
-        error: function(data) {
-            // consider doing more sophisticated error messages
+	callRegister()
+        .catch( (err) => {
+            console.log('errored');
+            console.log(err);
+            console.log(err.message);
             document.getElementById('last_name').innerHTML = 'Failed to create account';
-        }
-	});
+        })
+        .then( (result) => {
+            console.log('success');
+            console.log(result);
+            console.log(JSON.stringify(result));
+            //location.reload();
+        })
+}
+
+function callRegister() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/api/my/create',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/jsonp',
+            data: JSON.stringify({
+                "username": `${document.getElementById('username').value}`,
+                "password": `${document.getElementById('password').value}`,
+                "email": `${document.getElementById('email').value}`,
+                "first_name": `${document.getElementById('first_name').value}`,
+                "last_name": `${document.getElementById('last_name').value}`
+            }),
+            processData: false,
+            success: function (data) {
+                resolve(data);
+            },
+            error: function(data) {
+                // consider doing more sophisticated error messages
+                reject(data);
+            }
+        });
+    })
 }
