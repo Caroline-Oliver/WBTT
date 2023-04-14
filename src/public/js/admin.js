469215e14f1461b1c1662619f46382f5cded5696
Caroline-Oliver <me@carolineoliver.net>
2023-04-14 11:17:17 -0500

    Added create user function from admin portal

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -1,48 +1,61 @@
 // JavaScript Document
 
-var tabOrders=document.getElementById('orders');
-var tabUsers=document.getElementById('users');
-var tabTickets=document.getElementById('tickets');
-var tabVenues=document.getElementById('venues');
-var tabEvents=document.getElementById('events');
+var tabOrders = document.getElementById('orders');
+var tabUsers = document.getElementById('users');
+var tabTickets = document.getElementById('tickets');
+var tabVenues = document.getElementById('venues');
+var tabEvents = document.getElementById('events');
 
-tabOrders.addEventListener('click',function() {setView('orders-body')},true);
-tabUsers.addEventListener('click',function() {setView('users-body')},false);
-tabTickets.addEventListener('click',function() {setView('tickets-body')},false);
-tabVenues.addEventListener('click',function() {setView('venues-body')},false);
-tabEvents.addEventListener('click',function() {setView('events-body')},false);
+tabOrders.addEventListener('click', function () { setView('orders-body') }, true);
+tabUsers.addEventListener('click', function () { setView('users-body') }, false);
+tabTickets.addEventListener('click', function () { setView('tickets-body') }, false);
+tabVenues.addEventListener('click', function () { setView('venues-body') }, false);
+tabEvents.addEventListener('click', function () { setView('events-body') }, false);
 
 function createUser() {
-	// headers = {
-	// 	'Content-Type': 'application/json',
-	// }
+	callRegister()
+		.catch((err) => {
+			console.log('errored');
+			console.log(err);
+			console.log(err.message);
+			document.getElementById('error_box').innerHTML = 'Failed to create account';
+		})
+		.then((result) => {
+			console.log(result);
+			if (result != "Account successfully created!") {
+				document.getElementById('error-box').innerHTML = result;
+				document.getElementById('success-box').innerHTML = '';
+			}
+			else {
+				document.getElementById('error-box').innerHTML = '';
+				document.getElementById('success-box').innerHTML = result;
+			}
+		})
+}
 
-	// json_data = {
-	// 	'username': document.getElementById('username').value,
-	// 	'password': document.getElementById('password').value,
-	// 	'email': document.getElementById('email').value,
-	// 	'first_name': document.getElementById('first_name').value,
-	// 	'last_name': document.getElementById('last_name').value,
-	// }
-
-	const settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "http://18.219.2.17:3000/api/my/create",
-		"method": "POST",
-		"headers": {
-			"Content-Type": "application/json"
-		},
-		"processData": false,
-		"data": `{\n    \"username\" : \"${document.getElementById('username').value}\",\n    \"password\" : \"${document.getElementById('password').value}\",\n    \"email\" : \"${document.getElementById('email').value}\",\n    \"first_name\" : \"${document.getElementById('first_name').value}\",\n    \"last_name\" : \"${document.getElementById('last_name').value}\"\n}\n`
-	};
-
-	$.ajax(settings).done(function (response) {
-		console.log(response);
-
-	});
-	setMessage("This one worked")
-
+function callRegister() {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/api/my/create',
+			dataType: 'text',
+			type: 'get',
+			contentType: 'application/jsonp',
+			data: JSON.stringify({
+				"username": `${document.getElementById('username').value}`,
+				"password": `${document.getElementById('password').value}`,
+				"email": `${document.getElementById('email').value}`,
+				"first_name": `${document.getElementById('first_name').value}`,
+				"last_name": `${document.getElementById('last_name').value}`
+			}),
+			success: function (data) {
+				resolve(data);
+			},
+			error: function (data) {
+				// consider doing more sophisticated error messages
+				reject(data);
+			}
+		});
+	})
 }
 
 function setMessage(message) {
--- src/public/js/register.js
+++ src/public/js/register.js
@@ -3,14 +3,6 @@ function register() {
 	var sentinel = -1
 	window.localStorage.setItem('minutes', sentinel);
 	window.localStorage.setItem('seconds', sentinel);
-	
-    console.log(JSON.stringify({
-        "username": `${document.getElementById('username').value}`,
-        "password": `${document.getElementById('password').value}`,
-        "email": `${document.getElementById('email').value}`,
-        "first_name": `${document.getElementById('first_name').value}`,
-        "last_name": `${document.getElementById('last_name').value}`
-    }));
 
 	callRegister()
         .catch( (err) => {
