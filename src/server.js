09fed28066901ecfafb17dba6fa833b669668902
Caroline-Oliver <me@carolineoliver.net>
2023-04-14 11:41:45 -0500

    Fixed typo

--- src/server.js
+++ src/server.js
@@ -1283,18 +1283,18 @@ app.get('/api/admin/editUser', (req, res) => {
         }
     }
 
-    let user_sql = 'UPDATE user SET username=?, email=?, first_name=?, last_name=?, type=? WHERE user_id=?;';
+    let user_sql = 'UPDATE user SET user_name=?, email=?, first_name=?, last_name=?, type=? WHERE user_id=?;';
     let user_params = [username, email, first_name, last_name, type, user_id];
     let user_query = query(user_sql, user_params);
 
     if (password != null) {
-        let pass_sql = 'UPDATE password SET username=?, password=? WHERE password_id=?';
+        let pass_sql = 'UPDATE password SET user_name=?, password=? WHERE password_id=?';
         let pass_params = [username, password, user_id];
         let pass_query = query(pass_sql, pass_params);
     }
 
     else {
-        let pass_sql = 'UPDATE password SET username=? WHERE password_id=?';
+        let pass_sql = 'UPDATE password SET user_name=? WHERE password_id=?';
         let pass_params = [username, user_id];
         let pass_query = query(pass_sql, pass_params);
     }
