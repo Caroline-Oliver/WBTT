2eaf3eaa0989aac9ebea94778017f6c02b974f73
Caroline-Oliver <me@carolineoliver.net>
2023-04-14 11:36:14 -0500

    Added edit user

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -58,6 +58,53 @@ function callRegister() {
 	})
 }
 
+function changeUser() {
+	callEditUser()
+		.catch( (err) => {
+			console.log('errored');
+			console.log(err);
+			console.log(err.message);
+			document.getElementById('error_box').innerHTML = 'Failed to edit account';
+		})
+		.then( (result) => {
+			if (result != "Account edited successfully!") {
+				document.getElementById('error-box').innerHTML = result;
+				document.getElementById('success-box').innerHTML = '';
+			}
+			else {
+				document.getElementById('error-box').innerHTML = '';
+				document.getElementById('success-box').innerHTML = result;
+			}
+		})
+}
+
+function callEditUser() {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: '/api/admin/editUser',
+			dataType: 'text',
+			type: 'get',
+			contentType: 'application/jsonp',
+			data: JSON.stringify({
+				"user_id": `${document.getElementById('user_id').value}`,
+				"username": `${document.getElementById('username').value}`,
+				"password": `${document.getElementById('password').value}`,
+				"email": `${document.getElementById('email').value}`,
+				"first_name": `${document.getElementById('first_name').value}`,
+				"last_name": `${document.getElementById('last_name').value}`,
+				"type": `${document.getElementById('type').value}`
+			}),
+			success: function (data) {
+				resolve(data);
+			},
+			error: function (data) {
+				// consider doing more sophisticated error messages
+				reject(data);
+			}
+		});
+	});
+}
+
 function setMessage(message) {
 	var printThing = document.getElementById('results');
 	printThing.innerHTML = '<P>' + message + '</P>'//response;
--- src/server.js
+++ src/server.js
@@ -1246,6 +1246,61 @@ app.post('/api/admin/createTickets', (req, res) => {
 
 });
 
+app.get('/api/admin/editUser', (req, res) => {
+    var user_id, username, password, email, first_name, last_name, type;
+    // make sure request contains all elements of a user account
+    if (req.body.user_id != null && req.body.username != null
+        && req.body.password != null && req.body.email != null
+        && req.body.first_name != null && req.body.last_name != null
+        && req.body.type != null) {
+        user_id = req.body.user_id;
+        username = req.body.username;
+        password = req.body.password;
+        email = req.body.email;
+        first_name = req.body.first_name;
+        last_name = req.body.last_name;
+        type = req.body.type;
+    }
+    else {
+        var req_query = JSON.parse(Object.keys(req.query)[0]);
+        if (req_query.user_id != null && req_query.username != null
+            && req_query.password != null && req_query.email != null
+            && req_query.first_name != null && req_query.last_name != null
+            && req_query.type != null) {
+            user_id = req_query.user_id;
+            username = req_query.username;
+            password = req_query.password;
+            email = req_query.email;
+            first_name = req_query.first_name;
+            last_name = req_query.last_name;
+            type = req_query.type;
+        }
+        else {
+            res.status(403).send("Missing body parts");
+            return;
+        }
+    }
+    let user_sql = 'UPDATE user SET username=?, email=?, first_name=?, last_name=?, type=? WHERE user_id=?;';
+    let user_params = [username, email, first_name, last_name, type, user_id];
+    let user_query = query(user_sql, user_params);
+
+    let pass_sql = 'UPDATE password SET username=?, password=? WHERE password_id=?';
+    let pass_params = [username, password, user_id];
+    let pass_query = query(pass_sql, pass_params);
+
+    Promise.all([user_query, pass_query])
+        .catch( (err) => {
+            console.log('error in update user admin function');
+            console.log(err.message);
+            res.send('error in db');
+        })
+        .then( (result) => {
+            res.send('Account edited successfully!')
+        });
+
+
+});
+
 app.post('/api/admin/upload', authenticate, (req, res) => {
     fs.writeFile(`venues/${req.body.name}.html`, req.body.html, (err) => {
         if (err) return console.log(err);
