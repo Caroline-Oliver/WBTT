d1f23b56468814081161a6251325392d253becce
Caroline Oliver <me@carolineolive.net>
2023-04-21 15:02:10 -0500

    More logging

--- src/server.js
+++ src/server.js
@@ -862,12 +862,12 @@ app.get('/api/my/editAccount', (req, res) => {
             return;
         }
     }
-
+    console.log('made it here');
     let user_sql = 'UPDATE user SET user_name=?, email=?, first_name=?, last_name=? WHERE user_id=?;';
     let user_params = [username, email, first_name, last_name, type, user_id];
     let user_query = query(user_sql, user_params);
     let pass_query;
-
+    console.log('made it there');
     if (password != null && password != '') {
         let pass_sql = 'UPDATE password SET user_name=?, password=? WHERE password_id=?';
         let pass_params = [username, password, user_id];
@@ -879,7 +879,7 @@ app.get('/api/my/editAccount', (req, res) => {
         let pass_params = [username, user_id];
         let pass_query = query(pass_sql, pass_params);
     }
-
+    console.log('made it over here');
     Promise.all([user_query, pass_query])
         .catch((err) => {
             console.log('error in update user admin function');
