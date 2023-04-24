1aa0764d6261f6f3b4ac4cd876c8f9e6464a1ba7
click-clack09 <77758597+click-clack09@users.noreply.github.com>
2023-04-23 22:27:22 -0500

    Update event-builder.js

--- src/public/js/event-builder.js
+++ src/public/js/event-builder.js
@@ -73,7 +73,7 @@ function startDrag(evt) {
 	        textFields = document.getElementsByClassName('stage-label');
 		    
 		for (var i = 0; i<textFields.length; i++){
-			console.log("THIS-->"+textFields[i]);
+			console.log("THIS-->"+textFields[i].getAttribute('id'));
 	    	if (textFields[i] == current){
 	    		makeVisible('TextLabel');
 			makeVisible('labelEight');
